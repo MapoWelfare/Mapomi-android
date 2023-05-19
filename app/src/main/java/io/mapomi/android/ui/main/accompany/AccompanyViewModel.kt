c0e40e5d084ac3c6aad3b287a71f6d115d481946
rio319-an <78468001+rio319-an@users.noreply.github.com>
2023-05-20 00:31:35 +0900

    [05.30-안승우] 디자인 전면 수정

--- app/src/main/java/io/mapomi/android/ui/main/accompany/AccompanyViewModel.kt
+++ app/src/main/java/io/mapomi/android/ui/main/accompany/AccompanyViewModel.kt
@@ -1,10 +1,12 @@
 package io.mapomi.android.ui.main.accompany
 
+import android.widget.EditText
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.mapomi.android.enums.Page
 import io.mapomi.android.model.insets.SoftKeyModel
 import io.mapomi.android.ui.base.BaseViewModel
 import io.mapomi.android.ui.main.post.adapter.PostAdapter
+import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,23 +16,53 @@ class AccompanyViewModel @Inject constructor(
 
     val adapter = PostAdapter(::onItemClick)
 
+    val searchPageOn = MutableStateFlow(false)
+
 
     /*******************************************
      **** 버튼을 누릅니다
      ******************************************/
 
+    /**
+     * 검색바를 엽니다
+     */
+    fun onSearch()
+    {
+        searchPageOn.value = true
+    }
+
+    /**
+     * 검색바를 닫습니다
+     */
+    fun closeSearch()
+    {
+        soft.hideKeyboard()
+        searchPageOn.value = false
+    }
+
+    /**
+     * 검색합니다
+     */
+    fun searchText()
+    {
+        soft.hideKeyboard()
+    }
+
+    /**
+     * 리스트 아이템을 누릅니다
+     */
     private fun onItemClick()
     {
         navigation.changePage(Page.POST_DETAIL)
     }
 
+    /**
+     * + 버튼을 누릅니다
+     */
     fun onAddPost()
     {
         navigation.changePage(Page.POST_WRITE)
     }
 
-    fun searchText()
-    {
-        soft.hideKeyboard()
-    }
+
 }
