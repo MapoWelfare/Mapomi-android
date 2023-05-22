1f5e0b8b93e8d8813d05211b0b9fe43fbb36aba2
Andy Yun <andy000923@gmail.com>
2023-05-23 04:17:06 +0900

    [05.23-윤강현] 글 상세보기 페이지 프레임 추가

--- app/src/main/java/io/mapomi/android/ui/main/post/detail/PostDetailViewModel.kt
+++ app/src/main/java/io/mapomi/android/ui/main/post/detail/PostDetailViewModel.kt
@@ -3,7 +3,13 @@ package io.mapomi.android.ui.main.post.detail
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.mapomi.android.ui.base.BaseViewModel
 import javax.inject.Inject
+import android.widget.EditText
+import io.mapomi.android.model.post.PostModel
+import io.mapomi.android.ui.main.post.adapter.PostDateAdapter
+import io.mapomi.android.utils.TimeUtil
+import kotlinx.coroutines.flow.MutableStateFlow
 
 @HiltViewModel
-class PostDetailViewModel @Inject constructor() : BaseViewModel() {
+class PostDetailViewModel @Inject constructor() : BaseViewModel(
+) {
 }
